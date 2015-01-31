a00f8c205954016549f9be1662913d0f8a8e215a
Anthony Albertini <kiki@MacBook-Pro-de-Anthony.local>
2015-01-31 19:31:46 +0100

    feat(registerItemView): implements Backbone.Validation on the register form

--- app/scripts/views/registerItemView.js
+++ app/scripts/views/registerItemView.js
@@ -1,14 +1,24 @@
 define([
   'marionette',
+  'backbone.validation',
+  'behaviors/index',
   'templates'
 ],
 
-function (Marionette, templates) {
+function (Marionette, Validation, behaviors, templates) {
   'use strict';
 
   return Marionette.ItemView.extend({
     template: templates.register,
     className: 'form--register',
+
+    ui: {
+      submitButton: 'button[type="submit"]'
+    },
+
+    events: {
+      'click @ui.submitButton': 'signUp'
+    },
 
     behaviors: {
       PreventClick: {
@@ -18,6 +28,39 @@ function (Marionette, templates) {
 
     initialize: function () {
       console.log('initialize: registerItemView');
+
+      // https://github.com/thedersen/backbone.validation#validation-binding
+      // https://github.com/thedersen/backbone.validation#callbacks
+      Validation.bind(this, {
+        valid: function (view, attr) {
+          // do something
+          debugger
+        },
+        invalid: function (view, attr, error) {
+          // do something
+          debugger
+        }
+      });
+    },
+
+    onDestroy: function () {
+      // https://github.com/thedersen/backbone.validation#unbinding
+      Validation.unbind(this);
+    },
+
+    signUp: function () {
+      // var data = Backbone.Syphon.serialize(this);
+      // this.model.set(data);
+
+      this.model.set({
+        email: 'ddsfdd'
+      });
+
+      // https://github.com/thedersen/backbone.validation#isvalid
+      if (this.model.isValid(true)) {
+        // this.model.save();
+        console.log('Great Success!');
+      }
     }
   });
 });

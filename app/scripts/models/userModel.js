2e4da9f5503eace334fde3e3e587b9196f9d9ed4
Anthony Albertini <kiki@MacBook-Pro-de-Anthony.local>
2015-02-08 16:44:56 +0100

    feat(userModel): implements validated events

--- app/scripts/models/userModel.js
+++ app/scripts/models/userModel.js
@@ -17,6 +17,29 @@ function (Backbone, Validation, UserRules) {
 
     initialize: function () {
       console.log('initialize: UserModel');
+
+      // https://github.com/thedersen/backbone.validation#events
+
+      // https://github.com/thedersen/backbone.validation#validated
+      this.on('validated', this.validated, this);
+
+      // https://github.com/thedersen/backbone.validation#validatedvalid
+      this.on('validated:valid', this.validatedValid, this);
+
+      // https://github.com/thedersen/backbone.validation#validatedinvalid
+      this.on('validated:invalid', this.validatedInvalid, this);
+    },
+
+    validated: function (isValid, model, errors) {
+      debugger
+    },
+
+    validatedValid: function (model) {
+      debugger
+    },
+
+    validatedInvalid: function (model, errors) {
+      debugger
     }
   });
 });

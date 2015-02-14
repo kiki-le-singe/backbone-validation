af66ddc7d6ea897b2859948a3b5fd2e202d5cc05
Anthony Albertini <kiki@MacBook-Pro-de-Anthony.local>
2015-02-14 19:40:50 +0100

    style(init): fix indent style

--- app/scripts/init.js
+++ app/scripts/init.js
@@ -1,49 +1,49 @@
 require.config({
 
-    baseUrl: '/scripts',
+  baseUrl: '/scripts',
 
-    /* starting point for application */
-    deps: ['main', 'bootstrap'],
+  /* starting point for application */
+  deps: ['main', 'bootstrap'],
 
-    shim: {
-        bootstrap: {
-            deps: ['jquery']
-        },
-        backbone: {
-            deps: ['underscore', 'jquery']
-        },
-        marionette: {
-          deps: ['backbone']
-        }
+  shim: {
+    bootstrap: {
+      deps: ['jquery']
     },
-
-    paths: {
-        jquery: '../bower_components/jquery/dist/jquery',
-        underscore: '../bower_components/lodash/dist/lodash',
-        backbone: '../bower_components/backbone/backbone',
-        bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
-
-        /* backbone plugins */
-        'backbone.validation': '../bower_components/backbone-validation/dist/backbone-validation-amd',
-
-        /* alias all marionette libs */
-        marionette: '../bower_components/marionette/lib/backbone.marionette',
-        'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
-        'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
-        'backbone.syphon': '../bower_components/marionette.backbone.syphon/lib/backbone.syphon',
-
-        /* Alias text.js for template loading and shortcut the templates dir to tmpl */
-        text: '../bower_components/requirejs-text/text',
-        tmpl: '../templates',
-
-        /* Helpers */
-        helpersHBS: 'helpers/helpers',
-
-        /* requirejs-plugins */
-        json: '../bower_components/requirejs-plugins/src/json',
-
-        /* handlebars from the require handlerbars plugin below */
-        handlebars: '../bower_components/handlebars/handlebars',
-        hbs: '../bower_components/requirejs-hbs/hbs'
+    backbone: {
+      deps: ['underscore', 'jquery']
+    },
+    marionette: {
+      deps: ['backbone']
     }
+  },
+
+  paths: {
+    jquery: '../bower_components/jquery/dist/jquery',
+    underscore: '../bower_components/lodash/dist/lodash',
+    backbone: '../bower_components/backbone/backbone',
+    bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
+
+    /* backbone plugins */
+    'backbone.validation': '../bower_components/backbone-validation/dist/backbone-validation-amd',
+
+    /* alias all marionette libs */
+    marionette: '../bower_components/marionette/lib/backbone.marionette',
+    'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
+    'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
+    'backbone.syphon': '../bower_components/marionette.backbone.syphon/lib/backbone.syphon',
+
+    /* Alias text.js for template loading and shortcut the templates dir to tmpl */
+    text: '../bower_components/requirejs-text/text',
+    tmpl: '../templates',
+
+    /* Helpers */
+    helpersHBS: 'helpers/helpers',
+
+    /* requirejs-plugins */
+    json: '../bower_components/requirejs-plugins/src/json',
+
+    /* handlebars from the require handlerbars plugin below */
+    handlebars: '../bower_components/handlebars/handlebars',
+    hbs: '../bower_components/requirejs-hbs/hbs'
+  }
 });
